358e9bf70d1f51cb1ab6a5216e74cd8d2801e066
Francisco Sousa <francisco.sousa@hey.com>
2021-03-28 20:44:24 +0100

    wip

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,5 +1,3 @@
-import styler from "stylefire";
-
 // Make hash links smooth scroll
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
@@ -10,23 +8,3 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     });
   });
 });
-
-// Rotate stuff on scroll
-
-const shouldAnimate = window.matchMedia("(prefers-reduced-motion: reduce)");
-
-if (shouldAnimate)
-  window.addEventListener("scroll", scrollRotate, {
-    capture: true,
-    passive: false,
-  });
-
-function scrollRotate(event) {
-  const elements = document.querySelectorAll('[data-rotate-on-scroll="true"]');
-
-  elements.forEach((element) => {
-    styler(element).set({
-      rotate: (Number(element.dataset.rotateInitial) + window.scrollY) / 8,
-    });
-  });
-}
