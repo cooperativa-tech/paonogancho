02af84f8a710859a427e5213d78eb63dfe14ffe6
Francisco Sousa <francisco.sousa@hey.com>
2021-05-01 17:05:00 +0100

    Animate letters

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,5 @@
+import anime from "animejs/lib/anime.es.js";
+
 // Make hash links smooth scroll
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
@@ -8,3 +10,21 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     });
   });
 });
+
+// Animate title
+
+anime
+  .timeline({ loop: false })
+  .add({
+    targets: ".title-letter",
+    opacity: 1,
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 100 * i,
+  })
+  .add({
+    targets: ".title-opportunities",
+    opacity: 1,
+    duration: 1500,
+  });
