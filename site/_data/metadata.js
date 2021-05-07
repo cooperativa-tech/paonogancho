586394ab7b2e3267fcf304ef8c0e8911d3f85220
Francisco Sousa <francisco.sousa@hey.com>
2021-05-07 15:03:31 +0100

    Fix

--- site/_data/metadata.js
+++ site/_data/metadata.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: "Pão no Gancho",
-  description: "Coming soon",
+  description: "O Pão no Gancho é um gesto que pode mudar de uma forma positiva o dia de uma pessoa da sua rua, do seu bairro ou da sua cidade.",
   keywords: "coming soon",
   lang: "pt",
   host: "https://paonogancho.pt",
