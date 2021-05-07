65d13e63f656f262d1a472cc42dd330eb0c740cc
Francisco Sousa <francisco.sousa@hey.com>
2021-05-07 15:16:32 +0100

    Build

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -13,16 +13,14 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
 
 // Animate title
 
-anime
-  .timeline({ loop: false })
-  .add({
-    targets: ".title-letter",
-    opacity: 1,
-    translateY: ["0.5em", 0],
-    duration: 750,
-    delay: (el, i) => 75 * i,
-  })
-  /*.add({
+anime.timeline({ loop: false }).add({
+  targets: ".title-letter",
+  opacity: 1,
+  translateY: ["0.5em", 0],
+  duration: 750,
+  delay: (el, i) => 75 * i,
+});
+/*.add({
     targets: ".title-opportunities",
     opacity: 1,
     translateY: ["0.5em", 0],
@@ -30,7 +28,7 @@ anime
     delay: (el, i) => 100 * i,
   });*/
 
-const opportunities = document.querySelectorAll('.title-opportunities-anim');
+const opportunities = document.querySelectorAll(".title-opportunities-anim");
 opportunities.forEach((el, i) => {
   setTimeout(() => {
     el.style.transform = `translate(0px, 0px)`;
@@ -38,27 +36,27 @@ opportunities.forEach((el, i) => {
   }, 100 * i);
 });
 
-const navLinks = document.querySelector('nav').children[0].children;
+const navLinks = document.querySelector("nav").children[0].children;
 
-for(let i = 0; i < navLinks.length; i++){
+for (let i = 0; i < navLinks.length; i++) {
   setTimeout(() => {
     navLinks[i].style.transform = `translate(0px, 0px)`;
     navLinks[i].style.opacity = 1;
   }, 150 * i);
 }
 
-const faces = document.querySelectorAll('.face');
+const faces = document.querySelectorAll(".face");
 const rands = [];
-for(let i = 0; i < faces.length; i++){
+for (let i = 0; i < faces.length; i++) {
   const rand = Math.random();
-  if(rand > 0.5) rands.push(Math.random() * 0.5 + 0.5);
+  if (rand > 0.5) rands.push(Math.random() * 0.5 + 0.5);
   else rands.push(Math.random() * 0.5 - 1);
 }
 
-const reveal = document.querySelectorAll('.reveal');
+const reveal = document.querySelectorAll(".reveal");
 setTimeout(() => {
   faces.forEach((el, i) => {
-    if(i < 5){
+    if (i < 5) {
       setTimeout(() => {
         el.style.transform = `translate(0px, 0px)`;
         el.style.opacity = 1;
@@ -67,59 +65,65 @@ setTimeout(() => {
   });
 }, 700);
 
-const titles = document.querySelectorAll('.reveal-title svg');
+const titles = document.querySelectorAll(".reveal-title svg");
 
 setTimeout(() => {
   whenEntersViewport();
 }, 1200);
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   whenEntersViewport();
   whenScrolling();
 });
 
-function whenEntersViewport(){
+function whenEntersViewport() {
   faces.forEach((el, i) => {
-    if(i >= 5){
-      if(inViewport(el)){
+    if (i >= 5) {
+      if (inViewport(el)) {
         el.style.opacity = 1;
       }
     }
   });
 
   reveal.forEach((el, i) => {
-    if(inViewport(el)){
+    if (inViewport(el)) {
       el.style.transform = `translate(0px, 0px)`;
       el.style.opacity = 1;
     }
   });
 
   titles.forEach((el, i) => {
-    if(inViewport(el)){
+    if (inViewport(el)) {
       el.style.transform = `translate(0px, 0px)`;
       el.style.opacity = 1;
     }
   });
 }
 
-function whenScrolling(){
+function whenScrolling() {
   faces.forEach((el, i) => {
-    if(i >= 5){
-      el.style.transform = "translate(0px, 0px) rotate(" + window.pageYOffset/2 * rands[i] + "deg) scale(1)";
+    if (i >= 5) {
+      el.style.transform =
+        "translate(0px, 0px) rotate(" +
+        (window.pageYOffset / 2) * rands[i] +
+        "deg) scale(1)";
     }
   });
 }
 
 function inViewport(el) {
   let r, html;
-  if ( !el || 1 !== el.nodeType ) { return false; }
+  if (!el || 1 !== el.nodeType) {
+    return false;
+  }
   html = document.documentElement;
   r = el.getBoundingClientRect();
 
-  return ( !!r
-    && r.bottom >= html.clientHeight * 0.05
-    && r.right >= 0
-    && r.top <= html.clientHeight * 0.95
-    && r.left <= html.clientWidth
+  return (
+    !!r &&
+    r.bottom >= html.clientHeight * 0.05 &&
+    r.right >= 0 &&
+    r.top <= html.clientHeight * 0.95 &&
+    r.left <= html.clientWidth
   );
 }
